188a7ef8ff6a135de63e3df84b981e9d5462946c
Pubfred <pubfree@free.fr>
2018-11-26 12:47:23 +0100

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 11945 : 1945;
+    return testnet ? 62222 : 32222;
 }
 
 //
